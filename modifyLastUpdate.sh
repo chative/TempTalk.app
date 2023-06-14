effa3033bceeb4b1c1b662bedd4184b4575a152e
Ian.p <ian.p@b.com>
2023-06-14 11:35:55 +0300

    modify Last updated of a/hashes.html

--- modifyLastUpdate.sh
+++ modifyLastUpdate.sh
@@ -1,2 +1,2 @@
-gl;newData=`date '+%B %-d, %Y'`;sed -i'.bak' -e "s/Last updated : .*/Last updated : ${newData}/g" a/hashes.html
-git add -u ; git commit -m "modify Last updated of a/hashes.html"; gp
+git pull;newData=`date '+%B %-d, %Y'`;sed -i'.bak' -e "s/Last updated : .*/Last updated : ${newData}/g" a/hashes.html
+git add -u ; git commit -m "modify Last updated of a/hashes.html"; git push
