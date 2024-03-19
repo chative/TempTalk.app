3095f4f8c071044e33fc540e2b86810af8057f64
Clancey <8512327+clanceyz@users.noreply.github.com>
2024-03-19 15:25:24 +0800

    1.Fixed XSS vulnerabilities of landing pages. 2. Reduced the file size of ico file.

--- js/common.js
+++ js/common.js
@@ -20,3 +20,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+function htmlEscape(str){
+    return String(str).replace(/[^\w. ]/gi, function(c){
+        return '&#'+c.charCodeAt(0)+';';
+    });
+}
+
+function base64Escape(str) {
+    return (String(str).replace(/[^A-Za-z0-9+/=]/g, ''));
+}
